7a7fc8b8337749dbf0e3decf8002ff6bc1b8428a
Ath1thyan <athitasks@gmail.com>
2023-11-08 13:20:40 +0530

    nov 8

---
+++ arrays/PrimeNums.java
@@ -0,0 +1,12 @@
+package arrays;
+import arrays.PrimeLogic;
+
+public class PrimeNums {
+    public static void main(String[] args) {
+        int a[] = new int []{13,17,21,31,5,999991,23,25};
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+}
