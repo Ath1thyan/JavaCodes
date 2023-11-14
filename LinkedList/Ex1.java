967a66509f843cc3d97785a1cc4d7b36cf7e23c1
Ath1thyan <athitasks@gmail.com>
2023-11-15 05:18:11 +0530

    Prime num

---
+++ LinkedList/Ex1.java
@@ -0,0 +1,7 @@
+package Java.LinkedList;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        LogicClass ob = new LogicClass();
+    }
+}
---
+++ LinkedList/LogicClass.java
@@ -0,0 +1,35 @@
+package Java.LinkedList;
+
+public class LogicClass {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LogicClass(){
+        this.size = 0;
+    }
+
+    // Insert element first to the list
+    public void insertFirst(int value){
+        Node node = new Node(value, head);
+        head = node; // Update the head to the new node
+        if(tail==null){
+            tail=head;
+        }
+        size +=1;
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int val){
+            this.value = val;
+        }
+
+        public Node(int val, Node nex){
+            this.value = val;
+            this.next = nex;
+        }
+    }
+}
